a04a0b3e281bae1e242037fca4fce4e5f658bb61
JooYoung Seo <sjysky@gmail.com>
2019-12-08 12:14:19 -0500

    Update

--- lua/jy.lua
+++ lua/jy.lua
@@ -1,6 +1,5 @@
-  Str = function(el)
-    -- we don't count a word if it's entirely punctuation:
-    if el.text:match("Seo,") then
-        el.text:gsub("Seo,", "Kim,")
-    end
+function Str(el)
+  if el.text:match("Seo,") then
+    return pandoc.Strong(el)
   end
+end
