81ab159c54a8f0daf1716561cc66cfbb94401769
JooYoung Seo <jooyoung.j.seo@gmail.com>
2021-10-13 23:31:49 -0500

    Update

--- short_cv/update.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-mv *.tex ../docs/
-mv *.pdf ../docs/
-cd ..
-git add *
-git commit -m "Update"
-git push
-start https://jooyoungseo.github.io/jy_CV/JooYoung_Seo_Short_CV.pdf
-cd short_cv
--- update.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-git add *
-git commit -m "Update"
-git push
-start https://jooyoungseo.github.io/jy_CV/JooYoung_Seo_CV.pdf
